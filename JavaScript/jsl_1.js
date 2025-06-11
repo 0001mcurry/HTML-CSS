35ae68df8b3902d05519510da92de374e09acc71
Curry <1634325200197005@mil>
2025-06-11 13:20:24 -0400

    javascript folder added

---
+++ JavaScript/jsl_1.js
@@ -0,0 +1,56 @@
+// Here is a variable that's equal to a string
+var greeting = 'Hello world';
+
+// How to log a string value
+console.log(greeting);
+
+// Create a variable
+var numbersAdded = 5 + 5;
+
+// Log a variable again
+console.log(numbersAdded);
+
+var numGuest = 5 + 2;
+console.log(numGuest);
+
+var numGuest_1 = 10 * 5;
+console.log(numGuest_1);
+
+typeof 10;
+console.log(typeof numbersAdded);
+
+console.log(numGuest_1 != numGuest);
+
+console.log("Hi");
+
+var myname = 'Michael Curry';
+
+var welcome = `Welcome to JavaScript, ${myname}`;
+console.log(welcome);
+
+let a = true; let b = false;
+let answer = a || b;
+
+var numGuest = 5 + 2;
+console.log(numGuest);
+
+var firstName = 'Mike';
+var lastName  = 'Curry';
+console.log(firstName + lastName);
+
+const hasChocolate = false
+if (hasChocolate) {
+    console.log("Get Chocolate");
+} else {
+    console.log("Get Vanilla");
+}
+const outOfBread = true
+const outOfEggs = false
+console.log ("Time to go shopping!");
+if (outOfBread) {
+    console.log ("Buy Bread");
+}
+if (outOfEggs) {
+    console.log ("Buy Eggs");
+}
+
